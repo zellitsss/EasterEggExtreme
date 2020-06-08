36dd02b42c3762ae16b47fb9cffc57326911e907
zellitsss <zellitsss@gmail.com>
2020-06-08 19:36:05 +0700

    Update ServerSimulator

---
+++ assets/scripts/Defines.ts
@@ -0,0 +1,6 @@
+export const MAX_PLAYERS = 4;
+export const PLAY_TIME = 60; // in seconds
+
+// spawn time for 'eggs'
+export const MIN_SPAWN_TIME = 5;
+export const MAX_SPAWN_TIME = 8;
--- assets/scripts/ServerSimulator.ts
+++ assets/scripts/ServerSimulator.ts
@@ -1,14 +1,49 @@
+import { PLAY_TIME, MIN_SPAWN_TIME, MAX_SPAWN_TIME } from "./Defines";
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class ServerSimulator extends cc.Component {
-   // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    minUpdateTime: number = 0.1;
+    maxUpdateTime: number = 0.5;
 
-    start () {
+    updateCountdown: number = 0;
+    playTimeCountdown: number = 0;
 
+    spawnTime: number;
+
+    playersList = {};
+
+    onLoad() {
+        this.playTimeCountdown = PLAY_TIME;
+        this.spawnTime = this.GetSpawnTime();
     }
 
-    // update (dt) {}
+    update (dt) {
+
+        if (this.updateCountdown <= 0) {
+            // send update to client
+            this.updateCountdown = this.GetRandomUpdateTime();
+        } else {
+            this.updateCountdown -= dt;
+        }
+    }
+
+    UniqueID(): string {
+        let num = Date.now() + Math.random();
+        return num.toString(36);
+    }
+
+    RandomBetween(min: number, max: number, precision: number = 1): number {
+        return Math.floor(Math.random() * (max * precision - min * precision) + min * precision) / precision;
+    }
+
+    GetRandomUpdateTime(): number {
+        return this.RandomBetween(this.minUpdateTime, this.maxUpdateTime, 100);
+    }
+
+    GetSpawnTime(): number {
+        return this.RandomBetween(MIN_SPAWN_TIME, MAX_SPAWN_TIME);
+    }
 }
