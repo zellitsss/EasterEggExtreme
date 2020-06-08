ef3ac91257e49fc6702fbbbb397ad8fc3cf311be
zellitsss <zellitsss@gmail.com>
2020-06-08 19:07:24 +0700

    Add some basic scripts

---
+++ assets/scripts/LocalClient.ts
@@ -0,0 +1,14 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class LocalClient extends cc.Component {
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
---
+++ assets/scripts/RivalAI.ts
@@ -0,0 +1,14 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class RivalAI extends cc.Component {
+   // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
